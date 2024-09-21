e48b8126d92bd8d9c4eec5d44a1a614cb8a89fa6
Shubh Gupta <96350011+quietcod@users.noreply.github.com>
2024-09-21 05:40:51 +0530

    Top K Frequent Elements

---
+++ blind_75/top_k_frequent_element.py
@@ -0,0 +1,20 @@
+# Question - Given an integer array nums, and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+# Solution 
+
+class Solution:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
